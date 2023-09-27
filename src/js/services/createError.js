8ba50e29eb8773e3060a2f20d26f61bcace2f436
luizpbello <luizpaulopinto25@gmail.com>
2023-09-27 11:23:47 -0300

    feat: adicionando erro ao container

--- src/js/services/createError.js
+++ src/js/services/createError.js
@@ -1,7 +1,8 @@
 function createError(error) {
   const errorContainer = document.createElement("div");
-
+  errorContainer.classList.add('error-container')
   const errorMessage = document.createElement("span");
+
   errorMessage.textContent = error;
 
   errorContainer.appendChild(errorMessage);
