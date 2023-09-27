ccc45f043d2761d3f99434ab380fa271c127c46a
luizpbello <luizpaulopinto25@gmail.com>
2023-09-27 10:13:34 -0300

    feat: estrutura html e css

--- src/js/services/fetchData.js
+++ src/js/services/fetchData.js
@@ -1,8 +1,14 @@
 async function fetchDummies() {
-       const response = await fetch('https://dummyjson.com/users')
-       const data = await response.json()
-       return data
+  try {
+    const response = await fetch("https://dummyjson.com/users");
+    if (!response.ok) {
+      throw new Error("Erro ao buscar usuários.");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
 }
-
 
 export { fetchDummies };
