946f0793297826e52bfe3e36be6282c24dc2ccc0
luizpbello <luizpaulopinto25@gmail.com>
2023-09-27 10:28:12 -0300

    feat: função para criar card de usuários

--- src/js/services/createCards.js
+++ src/js/services/createCards.js
@@ -1,5 +1,21 @@
-
-
 async function createUserCard(user) {
-    
+  const userCard = document.createElement("div");
+  userCard.classList.add("user-card");
+
+  const userImage = document.createElement("img");
+  userImage.classList.add("user-image");
+
+  const userName = document.createElement("span");
+  userName.classList.add("user-name");
+
+  const userEmail = document.createElement("span");
+  userEmail.classList.add("user-email");
+
+  userCard.appendChild(userImage);
+  userCard.appendChild(userName);
+  userCard.appendChild(userEmail);
+
+  return userCard;
 }
+
+export { createUserCard };
